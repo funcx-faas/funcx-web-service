e9e00e8869b1913ee87bffedc62e04458f6d920f
Ubuntu <ubuntu@ip-172-31-78-145.ec2.internal>
2019-05-15 22:44:56 +0000

    fix bug in _resolve_endpoint

--- api/utils.py
+++ api/utils.py
@@ -147,7 +147,7 @@ def _register_site(user_id, endpoint_name, description):
 
     try:
         conn, cur = _get_db_connection()
-        endpoint_uuid = _resolve_endpoint(user_id, endpoint_name)
+        endpoint_uuid = _resolve_endpoint(user_id, endpoint_name, status='OFFLINE')
         if endpoint_uuid:
             return endpoint_uuid
         endpoint_uuid = str(uuid.uuid4())
@@ -161,7 +161,7 @@ def _register_site(user_id, endpoint_name, description):
     return endpoint_uuid
 
 
-def _resolve_endpoint(user_id, endpoint_name):
+def _resolve_endpoint(user_id, endpoint_name, status='ONLINE'):
     """
     Get the endpoint uuid from database
     """
@@ -169,7 +169,7 @@ def _resolve_endpoint(user_id, endpoint_name):
     endpoint_uuid = None
     try:
         conn, cur = _get_db_connection()
-        query = "select * from sites where status = 'ONLINE' and endpoint_name = '{}' and user_id = {} order by id DESC limit 1".format(endpoint_name, user_id)
+        query = "select * from sites where status = '{}' and endpoint_name = '{}' and user_id = {} order by id DESC limit 1".format(status, endpoint_name, user_id)
         cur.execute(query)
         r = cur.fetchone()
         endpoint_uuid = r['endpoint_uuid']
