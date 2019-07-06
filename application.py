88a9525621107e6aa09be1746731534e84725422
Ryan Chard <rchard@anl.gov>
2019-07-06 17:00:33 +0000

    Slowly migrating for EB

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import (Flask, request, flash, redirect, session, url_for)
 
-#from api.api import api
+from api.api import api
 #from api.automate_api import automate
 
 from config import SECRET_KEY, _load_funcx_client
@@ -11,8 +11,8 @@ application = Flask(__name__)
 
 
 # Include the API blueprint
-#app.register_blueprint(api, url_prefix="/api/v1")
-#app.register_blueprint(automate, url_prefix="/automate")
+application.register_blueprint(api, url_prefix="/api/v1")
+#application.register_blueprint(automate, url_prefix="/automate")
 
 @application.route("/")
 def hello():
