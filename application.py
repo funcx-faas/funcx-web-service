e08f97daaf39d4d9d2d2bf8a8360c482bc122578
Ryan <rchard@anl.gov>
2019-07-20 14:59:38 -0500

    Correcting routes to api

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ application = Flask(__name__)
 application.config.from_object(os.environ['APP_SETTINGS'])
 
 # Include the API blueprint
-application.register_blueprint(funcx_api, url_prefix="/routes/v1")
+application.register_blueprint(funcx_api, url_prefix="/api/v1")
 application.register_blueprint(automate_api, url_prefix="/automate")
 application.register_blueprint(auth_api)
 
