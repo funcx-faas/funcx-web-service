ff4182430326bc9d7506074992f8df315b1ae3f7
teresa <leeteresamaria@gmail.com>
2019-08-08 09:05:56 -0500

    try to make the drop-down for endpoints in funciton_view work + minor changes

--- gui/forms.py
+++ gui/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length
+# from models.utils import get_db_connection
+# from flask import current_app as app
 
 class EditForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired(), Length(min=1, max=20)])
@@ -15,3 +17,8 @@ class ExecuteForm(FlaskForm):
     endpoint = SelectField('Endpoint', choices=[('endpoint 1', 'Endpoint 1'), ('endpoint 2', 'Endpoint 2')], validators=[DataRequired()])
     payload = TextAreaField('Payload', validators=[DataRequired()])
     submit = SubmitField('Run')
+
+    # conn, cur = get_db_connection()
+    # cur.execute("SELECT endpoint_name, endpoint_uuid FROM sites WHERE endpoint_uuid IS NOT NULL AND user_id = %s;",
+    #             (user_id,))
+    # endpoints_list = cur.fetchall()
