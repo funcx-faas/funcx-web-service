956fdd9a3302e57a2857394f82e564d03922371d
Ryan <rchard@anl.gov>
2020-02-06 17:06:13 -0600

    Removing forms as a dependency broke.

--- gui/forms.py
+++ gui/forms.py
@@ -1,8 +1,9 @@
-from flask_wtf import FlaskForm
+# from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
-class EditForm(FlaskForm):
+# class EditForm(FlaskForm):
+class EditForm():
     name = StringField('Name', validators=[DataRequired(), Length(min=1, max=20)])
     desc = TextAreaField('Description', validators=[Length(min=0, max=500)])
     # language = SelectField('Language', choices=[('Python 3', 'python')], validators=[DataRequired()])
@@ -10,10 +11,12 @@ class EditForm(FlaskForm):
     code = TextAreaField('Code')
     submit = SubmitField('Save')
 
-class ExecuteForm(FlaskForm):
+# class ExecuteForm(FlaskForm):
+class ExecuteForm():
     endpoint = SelectField('Endpoint', choices=[], validators=[DataRequired()])
     data = TextAreaField('Payload', validators=[DataRequired()])
     submit = SubmitField('Run')
 
-class DeleteForm(FlaskForm):
+# class DeleteForm(FlaskForm):
+class DeleteForm():
     delete = SubmitField('Delete')
