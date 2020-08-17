af17f31cf8a7d9d2334da6597e1b52cd9bdac788
Ben Galewsky <ben@peartreestudio.net>
2020-08-17 11:43:56 -0500

    Add postman integration tests

--- integration_tests/get_valid_token.py
+++ integration_tests/get_valid_token.py
@@ -5,5 +5,5 @@ import json
 fxc = FuncXClient(funcx_service_address="http://localhost:5000/api/v1", force_login=False)
 token_file = os.path.join(fxc.TOKEN_DIR, fxc.TOKEN_FILENAME)
 with open(token_file) as f:
-  data = json.load(f)
-  print(data["funcx_service"]['access_token'])
+    data = json.load(f)
+    print(data["funcx_service"]['access_token'])
