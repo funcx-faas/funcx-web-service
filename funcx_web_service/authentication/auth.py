355960de76aba358575128521402ddcf1e233532
Ben Galewsky <ben@peartreestudio.net>
2020-10-14 11:46:12 -0500

    Migrate authorize_endpoint to SQL Alchemy

--- funcx_web_service/authentication/auth.py
+++ funcx_web_service/authentication/auth.py
@@ -1,3 +1,5 @@
+from funcx_web_service.models.auth_groups import AuthGroup
+from funcx_web_service.models.endpoint import Endpoint
 from funcx_web_service.models.utils import get_db_connection
 from flask import request, current_app as app
 import functools
@@ -114,50 +116,32 @@ def authorize_endpoint(user_id, endpoint_uuid, function_uuid, token):
     """
 
     authorized = False
-    try:
-        conn, cur = get_db_connection()
+    endpoint = Endpoint.find_by_uuid(endpoint_uuid)
+    authorized = False
 
-        # Check if the user owns the endpoint
-        query = "select * from sites where endpoint_uuid = %s"
-        cur.execute(query, (endpoint_uuid, ))
-        row = cur.fetchone()
-        app.logger.debug(f"Endpoint auth row: {row}")
+    if not endpoint:
+        raise Exception(
+            f"Endpoint {endpoint_uuid} not found")
 
-        # If the endpoint is flagged as protected we need to overwrite
+    if endpoint.restricted:
+        app.logger.debug("Restricted endpoint, checking function is allowed.")
+        whitelisted_functions = [f.function_uuid for f in endpoint.restricted_functions]
 
-        if len(row) > 0:
-            # If it is restricted we need to check the function is allowed, otherwise nothing else matters
-            if row['restricted']:
-                app.logger.debug("Restricted endpoint, checking function is allowed.")
-                query = "select * from restricted_endpoint_functions where endpoint_id = %s and function_id = %s"
-                cur.execute(query, (endpoint_uuid, function_uuid))
-                funcs = cur.fetchall()
-                app.logger.debug(f"Length of query response: {len(funcs)}")
-                if len(funcs) == 0:
-                    # There is no entry of this function, so reject it.
-                    raise Exception(f"Function {function_uuid} not permitted on endpoint {endpoint_uuid}")
+        if function_uuid not in whitelisted_functions:
+            raise Exception(f"Function {function_uuid} not permitted on endpoint {endpoint_uuid}")
 
-            # Check if the user owns it
-            if row['user_id'] == user_id:
-                authorized = True
-            # Otherwise if the row is public
-            elif row['public']:
-                authorized = True
+    if endpoint.public:
+        authorized = True
+    elif endpoint.user_id == user_id:
+        authorized = True
 
-        if not authorized:
-            # Check if there are any groups associated with this endpoint
-            query = "select * from auth_groups where endpoint_id = %s"
-            cur.execute(query, (endpoint_uuid,))
-            rows = cur.fetchall()
-            endpoint_groups = []
-            for row in rows:
-                endpoint_groups.append(row['group_id'])
-            if len(endpoint_groups) > 0:
-                authorized = check_group_membership(token, endpoint_groups)
+    if not authorized:
+        # Check if there are any groups associated with this endpoint
+        groups = AuthGroup.find_by_endpoint_uuid(endpoint_uuid)
+        endpoint_groups = [g.group_id for g in groups]
+        if len(endpoint_groups) > 0:
+            authorized = check_group_membership(token, endpoint_groups)
 
-    except Exception as e:
-        print(e)
-        app.logger.error(e)
     return authorized
 
 
