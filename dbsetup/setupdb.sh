55149e0ccceb20ec7abc02307799111b125b42d9
Ben Galewsky <ben@peartreestudio.net>
2020-10-20 14:00:26 -0500

    Create correct alembic migration file and remove dbsetup

--- dbsetup/setupdb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $(psql -h $POSTGRES_HOST public funcx -c "\dt" | wc | awk '{print $1}') -eq 20 ]]
-  then
-    echo "Existing database found";
-  else
-    psql -h $POSTGRES_HOST public funcx -f funcx-schema.sql;
-fi
