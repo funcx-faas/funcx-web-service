bd73d415a7f1f4946d8f26cedcfec7d1dcb7f99a
Ryan <rchard@anl.gov>
2021-03-12 09:53:48 -0600

    Fix test

--- tests/routes/test_funcx.py
+++ tests/routes/test_funcx.py
@@ -1,4 +1,3 @@
-from types import SimpleNamespace
 import pytest
 from funcx_web_service.models.container import Container
 from funcx_web_service.models.endpoint import Endpoint
@@ -57,21 +56,33 @@ def mock_redis(mocker):
 
 class TestFuncX(AppTestBase):
     def test_get_status(self, mock_auth_client, mock_redis, mocker):
-
         from funcx_web_service.models.tasks import Task
-        mock_task = SimpleNamespace(status="ready", result="foo", exception="bar",
-                                    completion_time=123)
+
+        get_rc = mocker.patch(
+            "funcx_web_service.routes.funcx.g_redis_client",
+            return_value=mock_redis)
+
+        rf_get = mocker.patch(
+            "funcx_web_service.models.tasks.RedisField.__get__",
+            return_value=True)
+
+        mock_expire = mocker.patch.object(Task, "_set_expire", return_value=123)
         mock_exists = mocker.patch.object(Task, "exists", return_value=True)
-        mock_from_id = mocker.patch.object(Task, "from_id", return_value=mock_task)
+
+        task = Task(mock_redis(), "42")
+        mock_from_id = mocker.patch.object(Task, "from_id", return_value=task)
 
         client = self.client
 
         result = client.get("/api/v1/tasks/42", headers={"Authorization": "my_token"})
 
-        # Need to get mock get_redis_client working
-        mock_exists.assert_called_with(None, "42")
-        mock_from_id.assert_called_with(None, "42")
-        assert result.json['status'] == 'ready'
+        mock_exists.assert_called()
+        mock_expire.assert_called()
+        mock_from_id.assert_called()
+        rf_get.assert_called()
+        get_rc.assert_called()
+
+        assert result.json["task_id"] == "42"
 
     def test_register_function(self, mock_auth_client, mocker):
         mock_ingest = mocker.patch("funcx_web_service.routes.funcx.ingest_function")
