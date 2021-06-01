dcb830f1b7d33a8ec6d0de719aa777d634f59869
yongyanrao <yongyan.rao@gmail.com>
2021-06-01 14:14:28 -0500

    Fixed unit tests

--- tests/routes/test_funcx.py
+++ tests/routes/test_funcx.py
@@ -1,4 +1,5 @@
 import pytest
+from funcx_web_service.routes.funcx import TaskGroup
 from funcx_web_service.models.container import Container
 from funcx_web_service.models.endpoint import Endpoint
 from funcx_web_service.models.function import Function
@@ -384,13 +385,14 @@ class TestFuncX(AppTestBase):
         assert result.json['code'] == int(ResponseErrorCode.UNKNOWN_ERROR)
         assert result.json['reason'] == "An unknown error occurred: hello"
 
-    @pytest.mark.skip(reason="The redis client is not mocked correctly, causing this to fail")
     def test_submit_function_access_forbidden(self, mocker, mock_auth_client):
         client = self.client
 
         mock_authorize_function = mocker.patch(
             "funcx_web_service.routes.funcx.authorize_function",
             return_value=False)
+
+        mocker.patch.object(TaskGroup, attribute='exists', return_value=False)
 
         result = client.post("api/v1/submit",
                              json={'tasks': [('1111', '2222', '')]},
@@ -405,13 +407,14 @@ class TestFuncX(AppTestBase):
         assert res['code'] == int(ResponseErrorCode.FUNCTION_ACCESS_FORBIDDEN)
         assert res['reason'] == "Unauthorized access to function 1111"
 
-    @pytest.mark.skip(reason="The redis client is not mocked correctly, causing this to fail")
     def test_submit_function_not_found(self, mocker, mock_auth_client):
         client = self.client
 
         mock_find_function = mocker.patch(
             "funcx_web_service.authentication.auth.Function.find_by_uuid",
             return_value=None)
+
+        mocker.patch.object(TaskGroup, attribute='exists', return_value=False)
 
         result = client.post("api/v1/submit",
                              json={'tasks': [('1111', '2222', '')]},
@@ -426,7 +429,6 @@ class TestFuncX(AppTestBase):
         assert res['code'] == int(ResponseErrorCode.FUNCTION_NOT_FOUND)
         assert res['reason'] == "Function 1111 could not be resolved"
 
-    @pytest.mark.skip(reason="The redis client is not mocked correctly, causing this to fail")
     def test_submit_endpoint_access_forbidden(self, mocker, mock_auth_client):
         client = self.client
 
@@ -441,6 +443,8 @@ class TestFuncX(AppTestBase):
         mock_authorize_endpoint = mocker.patch(
             "funcx_web_service.routes.funcx.authorize_endpoint",
             return_value=False)
+
+        mocker.patch.object(TaskGroup, attribute='exists', return_value=False)
 
         result = client.post("api/v1/submit",
                              json={'tasks': [('1111', '2222', '')]},
@@ -457,7 +461,6 @@ class TestFuncX(AppTestBase):
         assert res['code'] == int(ResponseErrorCode.ENDPOINT_ACCESS_FORBIDDEN)
         assert res['reason'] == "Unauthorized access to endpoint 2222"
 
-    @pytest.mark.skip(reason="The redis client is not mocked correctly, causing this to fail")
     def test_submit_endpoint_not_found(self, mocker, mock_auth_client):
         client = self.client
 
@@ -472,6 +475,8 @@ class TestFuncX(AppTestBase):
         mock_find_endpoint = mocker.patch(
             "funcx_web_service.authentication.auth.Endpoint.find_by_uuid",
             return_value=None)
+
+        mocker.patch.object(TaskGroup, attribute='exists', return_value=False)
 
         result = client.post("api/v1/submit",
                              json={'tasks': [('1111', '2222', '')]},
@@ -488,7 +493,6 @@ class TestFuncX(AppTestBase):
         assert res['code'] == int(ResponseErrorCode.ENDPOINT_NOT_FOUND)
         assert res['reason'] == "Endpoint 2222 could not be resolved"
 
-    @pytest.mark.skip(reason="The redis client is not mocked correctly, causing this to fail")
     def test_submit_function_not_permitted(self, mocker, mock_auth_client, mock_endpoint):
         client = self.client
 
@@ -503,6 +507,8 @@ class TestFuncX(AppTestBase):
         mock_find_endpoint = mocker.patch(
             "funcx_web_service.authentication.auth.Endpoint.find_by_uuid",
             return_value=mock_endpoint)
+
+        mocker.patch.object(TaskGroup, attribute='exists', return_value=False)
 
         result = client.post("api/v1/submit",
                              json={'tasks': [('1111', '2222', '')]},
