554cef443493893d1d03b86fbef03d98ab8ae7f7
Ben Galewsky <ben@peartreestudio.net>
2021-12-03 16:06:13 -0600

    Webservice receives build status updates from container service

--- funcx_web_service/container_service_adapter.py
+++ funcx_web_service/container_service_adapter.py
@@ -2,6 +2,8 @@ from urllib.parse import urljoin
 
 import requests
 
+from funcx import ContainerSpec
+
 
 class ContainerServiceAdapter:
     def __init__(self, service_url):
@@ -13,3 +15,15 @@ class ContainerServiceAdapter:
             return result.json()
         else:
             return {"version": "Service Unavailable"}
+
+    def submit_build(self, container_id: str, container_spec: dict):
+        build_request = {
+            "container_type": "Docker",
+            "container_id": container_id,
+            "apt": container_spec["apt"],
+            "pip": container_spec["pip"],
+            "conda": container_spec["conda"]
+        }
+        result = requests.post(urljoin(self.service_url, "build"),
+                               json=build_request)
+        print(result)
